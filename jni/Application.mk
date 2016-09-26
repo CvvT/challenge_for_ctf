6f080f2f61c6e4d446fbcbfaca51757a93842f7f
CwT <546052909@qq.com>
2016-09-26 21:03:01 +0800

    clean up

--- jni/Application.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-APP_ABI := armeabi
-
-NDK_TOOLCHAIN_VERSION := clang3.4-obfuscator
-
-include $(BUILD_SHARED_LIBRARY)
--- jni/my_fwrite_arm.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdio.h>
-
-extern size_t my_fwrite(const void* buffer, size_t size, size_t count, FILE* stream);
-
-size_t my_fwrite_arm(const void* buffer, size_t size, size_t count, FILE* stream) {
-	return my_fwrite(buffer, size, count, stream);
-}
